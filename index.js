16f4d9ea9a62d237d0d53719a9be852e8f6be85f
Monica Gerard <mlgvla@gmail.com>
2023-06-12 13:36:09 -0400

    renders recipe cards by cuisine

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//https://www.themealdb.com/api/json/v1/1/list.php?a=list
-
 // Elements
 const cuisineSelect = document.querySelector("#cuisines")
 const categorySelect = document.querySelector("#categories")
+const recipeContainer = document.querySelector(".recipe-container")
+
 
 // Function Calls
 getCuisines()
@@ -12,56 +12,77 @@ getCategories()
 cuisineSelect.addEventListener("change", getRecipesByCuisine)
 // categorySelect.addEventListener("change", getRecipesByCategory)
 
-
 // Dropdown Functions
 function getCuisines() {
-    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
-    .then(r => r.json())
-    .then(cuisines => renderCuisineOptions(cuisines.meals))
-    .catch(error => alert(error))
+   fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+      .then(r => r.json())
+      .then(cuisines => renderCuisineOptions(cuisines.meals))
+      .catch(error => alert(error))
 }
 
 function getCategories() {
-    fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-    .then(r => r.json())
-    .then(categories => renderCategoryOptions(categories.meals))
-    .catch(error => alert(error))
-    
+   fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+      .then(r => r.json())
+      .then(categories => renderCategoryOptions(categories.meals))
+      .catch(error => alert(error))
 }
 
 function renderCuisineOptions(cuisines) {
-    cuisines.forEach(cuisine => {
-        const option = document.createElement("option")
-        option.value = cuisine.strArea
-        option.textContent = cuisine.strArea
-        cuisineSelect.append(option)
-    });
+   cuisines.forEach(cuisine => {
+      const option = document.createElement("option")
+      option.value = cuisine.strArea
+      option.textContent = cuisine.strArea
+      cuisineSelect.append(option)
+   })
 }
 
 function renderCategoryOptions(categories) {
-    categories.forEach(category => {
-        const option = document.createElement("option")
-        option.value = category.strCategory
-        option.textContent = category.strCategory
-        categorySelect.append(option)
-    });
+   categories.forEach(category => {
+      const option = document.createElement("option")
+      option.value = category.strCategory
+      option.textContent = category.strCategory
+      categorySelect.append(option)
+   })
 }
 
 // Recipe Collection Functions
 
 function getRecipesByCuisine(e) {
-    const cuisine = e.target.value
+   const cuisine = e.target.value
 
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cuisine}`)
+   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cuisine}`)
       .then(r => r.json())
       .then(recipes => renderAllRecipes(recipes.meals))
       .catch(error => alert(error))
 }
 
 function renderAllRecipes(recipes) {
-    recipes.forEach(recipe => {
-        console.log(recipe)
-     })
-     cuisineSelect.value = ""
-     categorySelect.value = ""
+    recipeContainer.replaceChildren()
+   recipes.forEach(recipe => {
+      renderRecipeCard(recipe)
+   })
+   cuisineSelect.value = ""
+   categorySelect.value = ""
+}
+
+function renderRecipeCard(recipe) {
+   const {
+      idMeal: recipeId,
+      strMeal: recipeName,
+      strMealThumb: recipeImage,
+   } = recipe
+
+   console.log(recipe)
+   const cardDiv = document.createElement("div")
+   cardDiv.classList.add("card")
+   // add event listener to card
+
+   const image = document.createElement("img")
+   image.src = recipeImage
+
+   const title = document.createElement("h3")
+   title.textContent = recipeName
+
+   cardDiv.append(image, title)
+   recipeContainer.append(cardDiv)
 }
